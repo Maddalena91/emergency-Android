4b69445d163971b18dc5ee7e5f2fa3da65f9c32b
unknown <chiara.barzagli.studio@fitstic-edu.com>
2021-03-03 22:51:40 +0100

    caricamento progetto

---
+++ Emergency/app/src/main/java/it/fitstic/emergency/TutorialGpsActivity.java
@@ -0,0 +1,14 @@
+package it.fitstic.emergency;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TutorialGpsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tutorial_gps_activity);
+    }
+}
---
+++ Emergency/app/src/main/java/it/fitstic/emergency/TutorialNotificationActivity.java
@@ -0,0 +1,14 @@
+package it.fitstic.emergency;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TutorialNotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tutorial_notification_activity);
+    }
+}
