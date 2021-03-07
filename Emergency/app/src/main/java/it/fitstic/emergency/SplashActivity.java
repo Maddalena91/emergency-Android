73964ac22060e477305a4d4a25d3ebb6f50e169a
unknown <matteo.calafato.studio@fitstic-edu.com>
2021-03-07 15:29:52 +0100

    Splash screen

---
+++ Emergency/app/src/main/java/it/fitstic/emergency/SplashActivity.java
@@ -0,0 +1,18 @@
+package it.fitstic.emergency;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, TutorialActivity.class));
+        finish();
+    }
+}
