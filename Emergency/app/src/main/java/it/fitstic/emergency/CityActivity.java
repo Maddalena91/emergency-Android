df48a5ddae3694f073ec9c00e56ac5486b6a2313
unknown <chiara.barzagli.studio@fitstic-edu.com>
2021-03-09 10:34:55 +0100

    schermate 5-9

---
+++ Emergency/app/src/main/java/it/fitstic/emergency/CityActivity.java
@@ -0,0 +1,14 @@
+package it.fitstic.emergency;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.city_activity);
+    }
+}
---
+++ Emergency/app/src/main/java/it/fitstic/emergency/ProvinceActivity.java
@@ -0,0 +1,14 @@
+package it.fitstic.emergency;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProvinceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.province_activity);
+    }
+}
